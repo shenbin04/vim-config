ef0927176b8229c28f28952239aa94b5d03743b9
Bin Shen <bshen@norm.ai>
2026-02-03 13:40:49 -0500

    detect venv python

--- lua/config/mason.lua
+++ lua/config/mason.lua
@@ -21,4 +21,14 @@ vim.lsp.config('vtsls', {
 vim.lsp.config('pyright', {
   on_attach = base.on_attach,
   capabilities = base.capabilities,
+  before_init = function(_, config)
+    local root = config.root_dir
+    local python = root .. "/.venv/bin/python"
+
+    if vim.fn.executable(python) == 1 then
+      config.settings = config.settings or {}
+      config.settings.python = config.settings.python or {}
+      config.settings.python.pythonPath = python
+    end
+  end,
 })
